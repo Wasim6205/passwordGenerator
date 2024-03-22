d65810082d510e376b16a869eb8f1a7d31064088
Wasim <wasimbhai6205@gmail.com>
2024-03-22 23:20:21 +0530

    create passwordGenerator

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import "./App.css"
+import PasswordGenerator from './components/PasswordGenerator'
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center bg-slate-950 h-screen text-white'>
+      <PasswordGenerator />
+    </div>
+  )
+}
+
+export default App
